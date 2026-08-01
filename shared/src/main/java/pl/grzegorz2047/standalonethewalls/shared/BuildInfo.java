7704eb76d6a7aa2dc17433af729f23c05cfd1784
Grzegorz <grzegorz2047@gmail.com>
2026-08-01 19:00:52 +0200

    Use Sunderfront as product name

--- shared/src/main/java/pl/grzegorz2047/standalonethewalls/shared/BuildInfo.java
+++ shared/src/main/java/pl/grzegorz2047/standalonethewalls/shared/BuildInfo.java
@@ -2,7 +2,7 @@ package pl.grzegorz2047.standalonethewalls.shared;
 
 /** Shared build metadata that is safe to use in every module. */
 public final class BuildInfo {
-    public static final String PRODUCT_NAME = "Standalone The Walls";
+    public static final String PRODUCT_NAME = "Sunderfront";
 
     private BuildInfo() {
         throw new AssertionError("No instances");
