d29b5d40359d04ea73e9e31a5d3578b94beac680
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 13:19:04 +0200

    Add bounded identity command response codes

---
+++ server/src/main/java/pl/grzegorz2047/standalonethewalls/server/administration/identity/IdentityAdministrationResponseCode.java
@@ -0,0 +1,12 @@
+package pl.grzegorz2047.standalonethewalls.server.administration.identity;
+
+/** Stable top-level outcomes returned to a console or remote administration adapter. */
+public enum IdentityAdministrationResponseCode {
+    PERMISSION_DENIED,
+    HANDLES_LISTED,
+    BANS_LISTED,
+    HANDLE_INSPECTED,
+    BAN_INSPECTED,
+    HANDLE_MUTATION_COMPLETED,
+    BAN_MUTATION_COMPLETED
+}
