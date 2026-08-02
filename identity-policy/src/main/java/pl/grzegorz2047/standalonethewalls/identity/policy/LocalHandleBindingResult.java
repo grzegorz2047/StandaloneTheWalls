e92fe3eb4de09787eac473ea2767641152a4cd25
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 09:02:28 +0200

    Define atomic local binding outcomes

---
+++ identity-policy/src/main/java/pl/grzegorz2047/standalonethewalls/identity/policy/LocalHandleBindingResult.java
@@ -0,0 +1,8 @@
+package pl.grzegorz2047.standalonethewalls.identity.policy;
+
+/** Atomic local first-use binding outcome. */
+public enum LocalHandleBindingResult {
+    BOUND,
+    MATCHED,
+    CONFLICT
+}
