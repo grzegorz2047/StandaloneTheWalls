7cbafd8f21fbafbb66e695f036b917c18767abeb
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 06:02:26 +0200

    Add registry activation result

---
+++ identity-registry/src/main/java/pl/grzegorz2047/standalonethewalls/registry/RegistryActivationResult.java
@@ -0,0 +1,7 @@
+package pl.grzegorz2047.standalonethewalls.registry;
+
+/** Outcome of an atomic verified-snapshot activation attempt. */
+public enum RegistryActivationResult {
+    ACTIVATED,
+    UNCHANGED
+}
