d1c87b463a3f22c3fffd158f559f878311dd4744
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 10:05:50 +0200

    Add local handle audit actions

---
+++ identity-policy/src/main/java/pl/grzegorz2047/standalonethewalls/identity/policy/LocalHandleAuditAction.java
@@ -0,0 +1,8 @@
+package pl.grzegorz2047.standalonethewalls.identity.policy;
+
+/** Administrative mutation represented by one local identity audit event. */
+public enum LocalHandleAuditAction {
+    RESERVE,
+    UNBIND,
+    REBIND
+}
