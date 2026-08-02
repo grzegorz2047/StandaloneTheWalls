ccc675ac8eae6b4f5f9b522296e65128efde90ad
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 06:12:03 +0200

    Describe identity registry module

--- identity-registry/build.gradle.kts
+++ identity-registry/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     `java-library`
 }
 
+description = "Offline verification and monotonic activation of signed identity-registry snapshots"
+
 dependencies {
     api(project(":protocol"))
 
