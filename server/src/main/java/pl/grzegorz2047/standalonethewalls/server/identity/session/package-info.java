d4363dd9c3013380bf4e6499afc4fcafb2154987
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 18:33:53 +0200

    Document authenticated session admission package

---
+++ server/src/main/java/pl/grzegorz2047/standalonethewalls/server/identity/session/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Server-owned integration from authenticated TLS player sessions through identity policy into a
+ * bounded pre-lobby handoff queue.
+ */
+package pl.grzegorz2047.standalonethewalls.server.identity.session;
