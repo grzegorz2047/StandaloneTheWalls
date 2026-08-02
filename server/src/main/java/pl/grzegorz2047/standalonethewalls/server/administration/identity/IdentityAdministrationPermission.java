ed71b68aa001c9f1cc8076427ede352df0f5d7d5
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 14:11:19 +0200

    Add registry administration permission

--- server/src/main/java/pl/grzegorz2047/standalonethewalls/server/administration/identity/IdentityAdministrationPermission.java
+++ server/src/main/java/pl/grzegorz2047/standalonethewalls/server/administration/identity/IdentityAdministrationPermission.java
@@ -4,5 +4,6 @@ package pl.grzegorz2047.standalonethewalls.server.administration.identity;
 public enum IdentityAdministrationPermission {
     VIEW_IDENTITY,
     MANAGE_HANDLE_BINDINGS,
-    MANAGE_PLAYER_BANS
+    MANAGE_PLAYER_BANS,
+    MANAGE_REGISTRY
 }
