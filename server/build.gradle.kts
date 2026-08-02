471d91e4054479c5bce909c82baec7e07719ca98
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 14:41:27 +0200

    Depend on local identity cache and SQLite adapters

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -8,7 +8,9 @@ dependencies {
     implementation(project(":protocol"))
     implementation(project(":map-format"))
     implementation(project(":identity-registry"))
+    implementation(project(":identity-registry-file"))
     implementation(project(":identity-policy"))
+    implementation(project(":identity-policy-sqlite"))
 
     implementation(libs.slf4j.api)
     runtimeOnly(libs.logback.classic)
