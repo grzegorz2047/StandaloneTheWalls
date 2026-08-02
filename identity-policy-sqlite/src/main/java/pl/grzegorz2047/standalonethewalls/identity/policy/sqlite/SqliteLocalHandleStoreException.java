d3ae045e7f43667213e601a67f36a745a83a7234
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 10:49:47 +0200

    Add SQLite store exception serial version

--- identity-policy-sqlite/src/main/java/pl/grzegorz2047/standalonethewalls/identity/policy/sqlite/SqliteLocalHandleStoreException.java
+++ identity-policy-sqlite/src/main/java/pl/grzegorz2047/standalonethewalls/identity/policy/sqlite/SqliteLocalHandleStoreException.java
@@ -2,6 +2,8 @@ package pl.grzegorz2047.standalonethewalls.identity.policy.sqlite;
 
 /** Fail-closed persistence or schema failure in the SQLite local identity adapter. */
 public final class SqliteLocalHandleStoreException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public SqliteLocalHandleStoreException(String message, Throwable cause) {
         super(message, cause);
     }
