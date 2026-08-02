db87d6832318f2fb9d82b288ffdac2a90eb0203e
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 12:52:02 +0200

    Add identity policy to server admission gate

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     implementation(project(":game-domain"))
     implementation(project(":protocol"))
     implementation(project(":map-format"))
+    implementation(project(":identity-policy"))
 
     implementation(libs.slf4j.api)
     runtimeOnly(libs.logback.classic)
