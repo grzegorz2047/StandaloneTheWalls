79825804895c879b01423e37985dfa757e1858d9
Grzegorz <grzegorz2047@gmail.com>
2026-08-02 13:17:45 +0200

    Add identity administration permissions

---
+++ server/src/main/java/pl/grzegorz2047/standalonethewalls/server/administration/identity/IdentityAdministrationPermission.java
@@ -0,0 +1,8 @@
+package pl.grzegorz2047.standalonethewalls.server.administration.identity;
+
+/** Independent capabilities for local identity administration. */
+public enum IdentityAdministrationPermission {
+    VIEW_IDENTITY,
+    MANAGE_HANDLE_BINDINGS,
+    MANAGE_PLAYER_BANS
+}
