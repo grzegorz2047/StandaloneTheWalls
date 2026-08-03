ab9200d6f59b0bff416a97aa88625e5e10188c0b
Grzegorz <grzegorz2047@gmail.com>
2026-08-03 08:15:38 +0200

    feat(client): define Direct Connect UI phases

---
+++ client/src/main/java/pl/grzegorz2047/standalonethewalls/client/ui/directconnect/DirectConnectUiPhase.java
@@ -0,0 +1,17 @@
+package pl.grzegorz2047.standalonethewalls.client.ui.directconnect;
+
+/** Immutable presentation phases for the Direct Connect vertical slice. */
+public enum DirectConnectUiPhase {
+    FORM,
+    RESOLVING,
+    CONNECTING,
+    SECURING_TRANSPORT,
+    AUTHENTICATING,
+    WAITING_ADMISSION,
+    JOINING_LOBBY,
+    CONFIRMING_IDENTITY,
+    ADMISSION_REJECTED,
+    FAILED,
+    CONNECTED,
+    DISCONNECTED
+}
