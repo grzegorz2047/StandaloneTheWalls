d6f29681472a3fe13ed839cd6e10a5bdbb2c1961
Grzegorz <grzegorz2047@gmail.com>
2026-08-04 09:54:31 +0200

    feat(map-format): define preparation teams

---
+++ map-format/src/main/java/pl/grzegorz2047/standalonethewalls/mapformat/PreparationTeam.java
@@ -0,0 +1,9 @@
+package pl.grzegorz2047.standalonethewalls.mapformat;
+
+/** Stable team identifiers stored in preparation gameplay metadata. */
+public enum PreparationTeam {
+    GREEN,
+    BLUE,
+    RED,
+    YELLOW
+}
