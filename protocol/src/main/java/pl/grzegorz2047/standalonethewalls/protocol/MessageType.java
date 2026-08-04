95df7c62918597891c2995cdd02d26e601d2bd6d
Grzegorz <grzegorz2047@gmail.com>
2026-08-04 07:19:19 +0200

    [P0][Protocol] Reserve preparation spawn message type

--- protocol/src/main/java/pl/grzegorz2047/standalonethewalls/protocol/MessageType.java
+++ protocol/src/main/java/pl/grzegorz2047/standalonethewalls/protocol/MessageType.java
@@ -18,7 +18,8 @@ public enum MessageType {
     LOBBY_SELECT_TEAM(12, 16, Channel.RELIABLE),
     LOBBY_SET_READY(13, 16, Channel.RELIABLE),
     LOBBY_COMMAND_RESULT(14, 32, Channel.RELIABLE),
-    LOBBY_MATCH_SNAPSHOT(15, 64, Channel.RELIABLE);
+    LOBBY_MATCH_SNAPSHOT(15, 64, Channel.RELIABLE),
+    PREPARATION_SPAWN_ASSIGNMENT(16, 160, Channel.RELIABLE);
 
     private final int wireId;
     private final int maximumPayloadBytes;
@@ -59,6 +60,7 @@ public enum MessageType {
             case 13 -> Optional.of(LOBBY_SET_READY);
             case 14 -> Optional.of(LOBBY_COMMAND_RESULT);
             case 15 -> Optional.of(LOBBY_MATCH_SNAPSHOT);
+            case 16 -> Optional.of(PREPARATION_SPAWN_ASSIGNMENT);
             default -> Optional.empty();
         };
     }
