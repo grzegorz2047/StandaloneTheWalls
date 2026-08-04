97ad8d3403db6963ef9cb101cb94947c60c8bff4
Grzegorz <grzegorz2047@gmail.com>
2026-08-04 19:08:37 +0200

    Declare preparation scene graph exception identity

--- client/src/main/java/pl/grzegorz2047/standalonethewalls/client/preparation/PreparationSceneGraphException.java
+++ client/src/main/java/pl/grzegorz2047/standalonethewalls/client/preparation/PreparationSceneGraphException.java
@@ -2,6 +2,8 @@ package pl.grzegorz2047.standalonethewalls.client.preparation;
 
 /** Fail-closed error raised when verified GLB bytes cannot become a jMonkeyEngine scene graph. */
 public final class PreparationSceneGraphException extends Exception {
+    private static final long serialVersionUID = 1L;
+
     public PreparationSceneGraphException(String message, Throwable cause) {
         super(message, cause);
     }
