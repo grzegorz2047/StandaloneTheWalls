f239ba1640326d10d3d5e28b159b33b51a9d9431
Grzegorz <grzegorz2047@gmail.com>
2026-08-06 10:35:00 +0200

    Verify obstacle maps reach the server definition

---
+++ server/src/test/java/pl/grzegorz2047/standalonethewalls/server/preparation/VerifiedPreparationObstacleMapAdapterTest.java
@@ -0,0 +1,33 @@
+package pl.grzegorz2047.standalonethewalls.server.preparation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import pl.grzegorz2047.standalonethewalls.mapformat.MinimalPreparationBundle;
+import pl.grzegorz2047.standalonethewalls.mapformat.PreparationObstacleBox;
+import pl.grzegorz2047.standalonethewalls.mapformat.TwMapBundleLoader;
+import pl.grzegorz2047.standalonethewalls.mapformat.TwMapLoadPolicy;
+import pl.grzegorz2047.standalonethewalls.mapformat.VerifiedMapBundle;
+
+class VerifiedPreparationObstacleMapAdapterTest {
+    private static final TwMapLoadPolicy POLICY =
+            new TwMapLoadPolicy(2 * 1024 * 1024, 4 * 1024 * 1024, 16, 100);
+
+    @Test
+    void carriesAllMinimalWallBoxesIntoTheServerDefinition() throws Exception {
+        VerifiedMapBundle bundle =
+                TwMapBundleLoader.load(MinimalPreparationBundle.createArchive(), POLICY);
+
+        PreparationMapDefinition map = VerifiedPreparationMapAdapter.adapt(bundle);
+
+        assertThat(map.obstacleMap().boxes())
+                .extracting(PreparationObstacleBox::name)
+                .containsExactly(
+                        "CentralWallXCollision",
+                        "CentralWallZCollision",
+                        "EastWallCollision",
+                        "NorthWallCollision",
+                        "SouthWallCollision",
+                        "WestWallCollision");
+    }
+}
